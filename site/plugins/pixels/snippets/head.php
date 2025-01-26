24ccffb0243422d2b19900bf3f7e3d21347e054a
Nico Hoffmann <me@nhoffmann.com>
2025-01-26 18:55:15 +0100

    New favicon

--- site/plugins/pixels/snippets/head.php
+++ site/plugins/pixels/snippets/head.php
@@ -11,4 +11,3 @@
 
 <link rel="icon" type="image/png" href="<?= url('/assets/images/favicon.png') ?>">
 <link rel="icon" type="image/svg+xml" href="<?= url('/assets/images/favicon.svg') ?>">
-<link rel="mask-icon" href="<?= url('/assets/images/safari-mask-icon.svg') ?>" color="#000">
