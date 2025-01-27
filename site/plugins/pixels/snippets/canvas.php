9a1ba145b0241fa58bcf0f56e9f61321560296a9
Nico Hoffmann <me@nhoffmann.com>
2025-01-27 15:43:00 +0100

    Fix canvas after centering

--- site/plugins/pixels/snippets/canvas.php
+++ site/plugins/pixels/snippets/canvas.php
@@ -1,62 +1,67 @@
-<div v-cloak class="canvas" :data-preset="settings.preset" :style="{
+<div class="canvas-wrapper" :style="{
+	width: settings.width + 'px',
+	height: settings.height + 'px'
+}">
+	<div v-cloak class="canvas" :data-preset="settings.preset" :style="{
 		backgroundColor: settings.background,
 		width: settings.width + 'px',
 		height: settings.height + 'px',
 		color: settings.color,
-		transform: 'translate(-50%, -50%) scale(' +  settings.zoom + ')'
+		transform: 'scale(' +  settings.zoom + ')'
 	}">
 
-	<div
-		v-if="settings.pattern"
-		class="canvas-pattern"
-		:style="{ backgroundImage: 'url(' + patterns[settings.pattern] + ')' }"
-	></div>
-
-	<header class="canvas-header">
 		<div
-			class="canvas-headline"
-			:style="{
-				color: settings.color,
-				fontWeight: settings.fontWeight
-			}"
-			spellcheck="false"
-			contenteditable="true"
-			@input="settings.headline = $event.target.innerText"
-		>
-			{{ settings.headline }}
-		</div>
-		<div v-if="settings.logo" class="canvas-logo"><?= $logo ?></div>
-	</header>
+			v-if="settings.pattern"
+			class="canvas-pattern"
+			:style="{ backgroundImage: 'url(' + patterns[settings.pattern] + ')' }"
+		></div>
 
-	<div class="canvas-image" :data-rounded="settings.rounded" :data-shadow="settings.shadow" :style="{
-			top: settings.mt + 'rem',
-			right: settings.mr + 'rem',
-			bottom: settings.mb + 'rem',
-			left: settings.ml + 'rem',
-			...settings.corners,
-		}">
-		<template v-if="settings.browser">
-			<div class="canvas-browser bg-black flex items-center" style="--gap: .375rem; padding: .625rem">
-				<svg width="10" height="10">
-					<circle fill="var(--color-gray-700)" cx="5" cy="5" r="5" />
-				</svg>
-				<svg width="10" height="10">
-					<circle fill="var(--color-gray-700)" cx="5" cy="5" r="5" />
-				</svg>
-				<svg width="10" height="10">
-					<circle fill="var(--color-gray-700)" cx="5" cy="5" r="5" />
-				</svg>
+		<header class="canvas-header">
+			<div
+				class="canvas-headline"
+				:style="{
+					color: settings.color,
+					fontWeight: settings.fontWeight
+				}"
+				spellcheck="false"
+				contenteditable="true"
+				@input="settings.headline = $event.target.innerText"
+			>
+				{{ settings.headline }}
 			</div>
-		</template>
-		<template v-if="settings.image">
-			<div class="canvas-image-wrapper" @dblclick="selectImage">
-				<img
-					:data-y="settings.position.y"
-					:data-x="settings.position.x"
-					:src="settings.image"
-					:style="{ width: settings.scale + '%' }"
-				>
-			</div>
-		</template>
+			<div v-if="settings.logo" class="canvas-logo"><?= $logo ?></div>
+		</header>
+
+		<div class="canvas-image" :data-rounded="settings.rounded" :data-shadow="settings.shadow" :style="{
+				top: settings.mt + 'rem',
+				right: settings.mr + 'rem',
+				bottom: settings.mb + 'rem',
+				left: settings.ml + 'rem',
+				...settings.corners,
+			}">
+			<template v-if="settings.browser">
+				<div class="canvas-browser bg-black flex items-center" style="--gap: .375rem; padding: .625rem">
+					<svg width="10" height="10">
+						<circle fill="var(--color-gray-700)" cx="5" cy="5" r="5" />
+					</svg>
+					<svg width="10" height="10">
+						<circle fill="var(--color-gray-700)" cx="5" cy="5" r="5" />
+					</svg>
+					<svg width="10" height="10">
+						<circle fill="var(--color-gray-700)" cx="5" cy="5" r="5" />
+					</svg>
+				</div>
+			</template>
+			<template v-if="settings.image">
+				<div class="canvas-image-wrapper" @dblclick="selectImage">
+					<img
+						:data-y="settings.position.y"
+						:data-x="settings.position.x"
+						:src="settings.image"
+						:style="{ width: settings.scale + '%' }"
+					>
+				</div>
+			</template>
+		</div>
 	</div>
 </div>
